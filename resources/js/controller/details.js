e3a904d8df57c957a4372e7762eef76f5e8bf3ea
Preeti Aggarwal <Pree21@users.noreply.github.com>
2019-05-18 00:15:11 +0530

    Update details.js

--- resources/js/controller/details.js
+++ resources/js/controller/details.js
@@ -1,11 +1,16 @@
 //Modal Dialog start
 app.controller('DialogController', function($scope, $mdDialog, $interval) {
     $scope.status = '  ';
-    $scope.Timer = null;
+    //$scope.Timer = null;
     $scope.customFullscreen = false;
     $scope.showAdvanced = function(ev, beer) {
       $scope.objBeer = beer;
-      $scope.objBeer.ingredients.hops.map((item,index)=> item.state = "IDLE");
+      $scope.objBeer.ingredients.hops.map((item,index)=> {
+          item.state = "IDLE";
+          item.timer = "";
+          item.isDisabled = true;
+          item.duration = $scope.objBeer.method.mash_temp[0].duration;
+      });
       $mdDialog.show({
           controller: DialogController,
           templateUrl: 'details.html',
@@ -16,12 +21,12 @@ app.controller('DialogController', function($scope, $mdDialog, $interval) {
           scope: $scope.$new()
       })
   };
-  $scope.changeState=function(hops,time){
-      var duration = time[0].duration;
+  $scope.changeState=function(hops){
+      var duration = hops.duration;
       if(duration != null && duration != '' && duration != undefined){
           if(hops.state == "Running"){
               hops.state = "Pause";
-              $scope.StopTimer();
+              $scope.StopTimer(hops.timer);
           }else{
               hops.state = "Running";
               $scope.StartTimer(hops, duration);
@@ -33,18 +38,20 @@ app.controller('DialogController', function($scope, $mdDialog, $interval) {
   //Timer start function.
   $scope.StartTimer = function (hops, duration) {
       //Initialize the Timer to run every 1000 milliseconds i.e. one second.
-      $scope.Timer = $interval(function () {
-          duration = duration-1;
-          if(duration <= 0){
-              $scope.StopTimer();
-              hops.state = "Done";
+      hops.timer = $interval(function () {
+          if(hops.duration > 0){
+            duration = duration-1;
+            hops.duration = duration;
+          }else{
+            hops.state = "Done";
+            $scope.StopTimer(hops.timer);
           }
       }, 1000);
   };
-  $scope.StopTimer = function () {
+  $scope.StopTimer = function (Timer) {
       //Cancel the Timer.
-      if (angular.isDefined($scope.Timer)) {
-          $interval.cancel($scope.Timer);
+      if (angular.isDefined(Timer)) {
+          $interval.cancel(Timer);
       }
   };
 });
